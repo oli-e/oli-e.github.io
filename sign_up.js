27edeb61ab7305425d516f24d9c68d500aa40e33
olena <olichkachorna@gmail.com>
2021-04-22 00:22:11 +0200

    index/css/signup

---
+++ sign_up.js
@@ -0,0 +1,25 @@
+const auth = firebase.auth();
+
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+const saveButton = document.querySelector("#saveButton");
+
+saveButton.addEventListener("click", function () {
+  const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+  console.log(location);
+  // location.replace("https://pros.rafalpietrzak.repl.co/home.html");
+  console.log()
+  window.alert("Signed Up!")
+});
+
+
+// auth.onAuthStateChanged(function(user) {
+//   if (user) {
+//     var email = user.email;
+//     window.alert();
+//     console.log('user logged in: ', user);
+//   } else {
+//     console.log('user logged out');
+//   }
+// });
