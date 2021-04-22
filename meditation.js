73a98945d082acb82430ef0016bcadceed82b48d
olena <olichkachorna@gmail.com>
2021-04-22 16:35:19 +0200

    quick fixes

--- meditation.js
+++ meditation.js
@@ -1,8 +1,11 @@
-function playSoundBeach(){
+function playSoundBeach() {
   var audio = new Audio('./sounds/beach.mp3');
   audio.play();
+  if (!audio.paused){
+    audio.pause();
+    
+  }
 }
-
 function playSoundRain(){
   var audio = new Audio('./sounds/rain.mp3');
   audio.play();
