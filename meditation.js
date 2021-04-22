c3b7f4855e9af20457442436488d754681f5174b
Olena <57922241+oli-e@users.noreply.github.com>
2021-04-22 17:13:32 +0200

    Update meditation.js

--- meditation.js
+++ meditation.js
@@ -1,16 +1,11 @@
 function playSoundBeach() {
   var audio = new Audio('./sounds/beach.mp3');
   audio.play();
-  if (!audio.paused){
-    audio.pause();
-    
-  }
 }
 function playSoundRain(){
   var audio = new Audio('./sounds/rain.mp3');
   audio.play();
 }
-
 function playSoundBirds(){
   var audio = new Audio('./sounds/birds.mp3');
   audio.play();
