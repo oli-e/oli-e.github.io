0874495d7a080d61f44e902f9fe723971e166bdb
olena <olichkachorna@gmail.com>
2021-04-22 15:08:41 +0200

    preventDefault

--- sign_up.js
+++ sign_up.js
@@ -5,15 +5,15 @@ const password = document.querySelector("#password");
 
 const saveButton = document.querySelector("#saveButton");
 
-saveButton.addEventListener("click", function () {
+saveButton.addEventListener("click", function (e) {
   const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
 
   
   promise.catch((error) => {
+    e.preventDefault();
     var errorCode = error.code;
     var errorMessage = error.message;
     window.alert(errorMessage);
-    // preventdefault
   });
 
 });
